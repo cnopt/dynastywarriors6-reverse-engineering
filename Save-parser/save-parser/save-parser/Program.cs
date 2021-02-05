4e099a9943c6437c7622291194c4db5b9cb0db09
chad <cbiddisc@gmail.com>
2021-02-05 20:09:35 +0000

    holy shit finally figured it out
    reading in the address as int16 using ReadInt16() finally returns the correct value

--- Save-parser/save-parser/save-parser/Program.cs
+++ Save-parser/save-parser/save-parser/Program.cs
@@ -22,12 +22,18 @@ class Program
         // To read data at specific byte: Address = Seek offset + number of bytes
         // i.e. to get data at offset 100 = Seek offset 99, as number of bytes is set to 1
         // if changing the size of bytes, adjust offset up or down
+        // holy shit i finally figured it out
+        // was using reader.read(), was giving me a random fucking idiot number.
+        // even reversing the endianness was still giving out random shit
+        // tried converting to int16/int32 AFTER read(), was still giving the same result. 
+        // reserved the endianness WHILE ALSO reading it as an int16 gave out -26330, which is
+        // 9981 in big endian. so i reversed and back and bam, 9881 returned
 
         using (BinaryReader reader = new BinaryReader(new FileStream(file, FileMode.Open)))
         {
             reader.BaseStream.Seek(4738, SeekOrigin.Begin); // where to start reading from 
             reader.Read(test, 0, numBytes); // number of bytes to read
-            var val1 = Decimal.ToInt32(reader.Read());
+            var val1 = reader.ReadInt16();
 
  
 
