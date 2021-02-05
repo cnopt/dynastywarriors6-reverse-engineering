4266986b420e89ed1464a58efb264f1f05af55e0
chad <cbiddisc@gmail.com>
2021-02-05 18:03:59 +0000

    c# save file parser
    using filestream to read in the binary data from the .dat file DW6 uses to store save data. not sure of specific locations to read just yet, but all in time

---
+++ Save-parser/save-parser/save-parser/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        R();
+    }
+
+    static void R()
+    {
+        string file = @"C:\Users\TTGCh\Desktop\dw6-saves\new-save.dat";
+
+        // read all
+        FileStream fs = new FileStream(file, FileMode.Open);
+        int hexIn;
+        String hex;
+        
+        for (int i = 0; (hexIn = fs.ReadByte()) != -1; i++)
+        {
+            hex = string.Format("{0:X2}", hexIn);
+            Console.Write(hex);
+        }
+
+
+
+
+
+        while (true) ;
+    }
+}
+
