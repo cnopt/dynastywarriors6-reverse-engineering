b036ea1deaa5ab09db826fa439b0849cf89b9906
chad <cbiddisc@gmail.com>
2021-02-05 18:28:31 +0000

    navigating multiple byte addresses
    probably not the correct way to do it at all, but can now navigate around the file to read multiple addresses without closing/opening it again

--- Save-parser/save-parser/save-parser/Program.cs
+++ Save-parser/save-parser/save-parser/Program.cs
@@ -15,14 +15,25 @@ class Program
     static void R()
     {
         string file = @"C:\Users\TTGCh\Desktop\dw6-saves\new-save.dat";
-        int numBytes = 2;
+        int numBytes = 1;
         byte[] test = new byte[numBytes];
+
+        // To read data at specific byte: Address = Seek offset + number of bytes
+        // i.e. to get data at offset 100 = Seek offset 99, as number of bytes is set to 1
+        // if changing the size of bytes, adjust offset up or down
 
         using (BinaryReader reader = new BinaryReader(new FileStream(file, FileMode.Open)))
         {
-            reader.BaseStream.Seek(98, SeekOrigin.Begin); // where to start reading from 
+            reader.BaseStream.Seek(99, SeekOrigin.Begin); // where to start reading from 
             reader.Read(test, 0, numBytes); // number of bytes to read
-            Console.WriteLine(reader.Read());
+            var val1 = reader.Read();
+
+            reader.BaseStream.Seek(103, SeekOrigin.Begin); // where to start reading from 
+            reader.Read(test, 0, numBytes); // number of bytes to read
+            var val2 = reader.Read();
+
+            Console.WriteLine(val1);
+            Console.WriteLine(val2);
         }
 
         // read all
