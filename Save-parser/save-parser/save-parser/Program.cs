b8f088d18b4d8a94fc46252cd3b58ee25f121599
chad <cbiddisc@gmail.com>
2021-02-05 18:19:05 +0000

    read bytes at specific address/offset

--- Save-parser/save-parser/save-parser/Program.cs
+++ Save-parser/save-parser/save-parser/Program.cs
@@ -15,17 +15,27 @@ class Program
     static void R()
     {
         string file = @"C:\Users\TTGCh\Desktop\dw6-saves\new-save.dat";
+        int numBytes = 2;
+        byte[] test = new byte[numBytes];
+
+        using (BinaryReader reader = new BinaryReader(new FileStream(file, FileMode.Open)))
+        {
+            reader.BaseStream.Seek(98, SeekOrigin.Begin); // where to start reading from 
+            reader.Read(test, 0, numBytes); // number of bytes to read
+            Console.WriteLine(reader.Read());
+        }
 
         // read all
-        FileStream fs = new FileStream(file, FileMode.Open);
-        int hexIn;
-        String hex;
-        
-        for (int i = 0; (hexIn = fs.ReadByte()) != -1; i++)
-        {
-            hex = string.Format("{0:X2}", hexIn);
-            Console.Write(hex);
-        }
+        //FileStream fs = new FileStream(file, FileMode.Open);
+        //int hexIn;
+        //String hex;
+        //
+        //for (int i = 0; (hexIn = fs.ReadByte()) != -1; i++)
+        //{
+        //    hex = string.Format("{0:X2}", hexIn);
+        //    Console.Write(hex);
+        //}
+
 
 
 
