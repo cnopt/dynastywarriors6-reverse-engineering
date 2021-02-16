4f4d7f17344306ca14dff9073a0cc08a71e8c7e0
chad <cbiddisc@gmail.com>
2021-02-16 17:25:06 +0000

    fill up weapons dictionary

--- Save-parser/save-parser/save-parser/Program.cs
+++ Save-parser/save-parser/save-parser/Program.cs
@@ -7,10 +7,7 @@ using System.IO;
 using System.Buffers.Binary;
 
 class Program
-
 {
-    
-
     static void Main(string[] args)
     {
 
@@ -29,11 +26,12 @@ class Program
 
 
         string file = @"C:\Users\TTGCh\Desktop\dw6-saves\save.dat";
-        //string officer = args[0];
+        // string file2 = args[0]; could also maybe look for it automatically in documents folder
+
         int numBytes = 4;
         byte[] test = new byte[numBytes];
 
-        var officerDict = new Dictionary<int, string>()
+        var officersDict = new Dictionary<int, string>()
         {
             {0, "Xiahou Dun"},
             {1, "Dian Wei"},
@@ -77,30 +75,157 @@ class Program
             {39, "Xiao Qiao"},
             {40, "Yue Ying"}
         };
-        var titleDict = new Dictionary<int, string>()
+        var titlesDict = new Dictionary<int, string>()
         {
             {0, "Lt. General" },
             {1, "Assisstant General" },
             {2, "Field General" },
         };
+        var weaponsDict = new Dictionary<int, string>()
+        {
+            { 00, "Rock Crusher"},
+            { 01, "Wave Breaker"},
+            { 02, "Thundersmash"},
+            { 03, "Violent Soul Flail"},
+            { 04, "Lion's Head Flail"},		
+            { 05, "Beserker Flail"},
+            { 06, " Eradication Claws"},
+            { 07, "Anguish Claws"},
+            { 08, "Necrosis Claws"},
+            { 09, "Twin Vipers"},
+            { 10, "Twin Dragons"},
+            { 11, "Twin Eagles"},
+            { 12, "Sword of Heaven"},
+            { 13, "Blue Blade"},
+            { 14, "Seven Star Sword"},
+            { 15, "Red Dusk"},
+            { 16, "Dark Night"},
+            { 17, "Scarlet Dawn"},
+            { 18, "Silver Swallow"},
+            { 19, "Blue Falcon"},
+            { 20, "Jade Warbler"},
+            { 21, "Madder Rose"},
+            { 22, "Wisteria Breeze"},
+            { 23, "Lotus Bow"},
+            { 24, "Crescent Moon"},
+            { 25, "Dancing Dragon"},
+            { 26, "Wing Blade"},
+            { 27, "Elder Sword"},
+            { 28, "Nine Hook Sword"},
+            { 29, "Golden Pheonix"},
+            { 30, "Dragon Spike"},
+            { 31, "Dragon Fang"},
+            { 32, "Dragon Talon"},
+            { 33, "Blue Dragon"},
+            { 34, "Black Dragon"},
+            { 35, "White Dragon"},
+            { 36, "Serpent Blade"},
+            { 37, "Python Blade"},
+            { 38, "Viper Blade"},
+            { 39, "Brilliance"},
+            { 40, "Distinction"},
+            { 41, "Enlightenment"},
+            { 42, "Strength and Virtue"},
+            { 43, "Heaven and Earth"},
+            { 44, "Yin and Yang"},
+            { 45, "Moonflower"},
+            { 46, "Dewflower"},
+            { 47, "Rainflower"},
+            { 48, "Sky Piercer"},
+            { 49, "Demon Bane"},
+            { 50, "Heron Blade Halberd"},
+            { 51, "Bone Crusher"},
+            { 52, "Chaos Crusher"},
+            { 53, "Whirlwind Crusher"},
+            { 54, "Heavens Destroyer"},
+            { 55, "Heavens Smasher"},
+            { 56, "Heavens Cutter"},
+            { 57, "Destroyer"},
+            { 58, "Annihilator"},
+            { 59, "Obliterator"},
+            { 60, "Splendor"},
+            { 61, "Mystery"},
+            { 62, "Ostentation"},
+            { 63, "Phoenix Wing"},
+            { 64, "Dragon Scale"},
+            { 65, "Tortoise Bite"},
+            { 66, "Havoc"},
+            { 67, "Mayhem"},
+            { 68, "Chaos"},
+            { 69, "Rage Trident"},
+            { 70, "Savage Trident"},
+            { 71, "Tsunami Trident"},
+            { 72, "Valor"},
+            { 73, "Spirit"},
+            { 74, "Courage"},
+            { 75, "River Slicer"},
+            { 76, "Mountain Breaker"},
+            { 77, "Sky Lasher"},
+            { 78, "Flashstrike"},
+            { 79, "Dawnstrike"},
+            { 80, "Duskstrike"},
+            { 81, "Flying Nimbus"},
+            { 82, "Rising Nimbus"},
+            { 83, "Lofting Nimbus"},
+            { 84, "Tryant Strike"},
+            { 85, "Glimmer Strike"},
+            { 86, "Stoic Strike"},
+            { 87, "Dragon's Might"},		
+            { 88, "Heaven's Might"},		
+            { 89, "Tiatan's Might"},	
+            { 90, "Ironhorse Glaive"},
+            { 91, "Dragonrun Glaive"},
+            { 92, "Warsteed Glaive"},
+            { 93, "Immortal Blade"},
+            { 94, "Battle Master Blade"},
+            { 95, "Princeps Blade"},
+            { 96, "The Awakener"},
+            { 97, "Bone Splitter"},
+            { 98, "Stormhowl"},
+            { 99, "Blue Dragon Ji"},
+            { 100, "Black Dragon Ji"},
+            { 101, "White Dragon Ji"},
+            { 102, "Firestorm Staff"},
+            { 103, "Blizzard Staff"},
+            { 104, "Typhoon Staff"},
+            { 105, "Wizard Club"},
+            { 106, "Magus Club"},
+            { 107, "Augur Club"},
+            { 108, "Sword of Kings"},
+            { 109, "Sword of Severity"},
+            { 110, "North Star Sword"},
+            { 111, "Blaze Staff"},
+            { 112, "Blight Staff"},
+            { 113, "Judgement Staff"},
+            { 114, "Allure"},
+            { 115, "Charm"},
+            { 116, "Seduction"},
+            { 117, "True"},
+            { 118, "True Beauty"},
+            { 119, "True Luster"},
+            { 120, "Emerald Dew"},
+            { 121, "Emerald Veil"},
+            { 122, "Emerald Mist"},
+            { 174, "Empty" }
+        };
 
 
         using (BinaryReader reader = new BinaryReader(new FileStream(file, FileMode.Open)))
         {
-            int xpBaseStart         = 3052;
-            int idBaseStart         = 3036;
-            int killsBaseStart      = 3056;
-            int costumeBaseStart    = 3040;
-            int titleBaseStart      = 3044;
-            int levelBaseStart      = 3048;
-            int weapon1BaseStart    = 2908;
-            int weapon2BaseStart    = 2924;
-            int weapon3BaseStart    = 2940;
-            int weapon4BaseStart    = 2956;
-            int weapon5BaseStart    = 2972;
-            int weapon6BaseStart    = 2988;
-            int weapon7BaseStart    = 3004;
-            int weapon8BaseStart    = 3020;
+            int xpBaseStart = 3052;
+            int idBaseStart = 3036;
+            int killsBaseStart = 3056;
+            int costumeBaseStart = 3040;
+            int titleBaseStart = 3044;
+            int levelBaseStart = 3048;
+            int weapon1BaseStart = 2908;
+            int weapon2BaseStart = 2924;
+            int weapon3BaseStart = 2940;
+            int weapon4BaseStart = 2956;
+            int weapon5BaseStart = 2972;
+            int weapon6BaseStart = 2988;
+            int weapon7BaseStart = 3004;
+            int weapon8BaseStart = 3020;
 
             for (int i = 0; i < 6888; i++)
             {
@@ -109,7 +234,11 @@ class Program
 
                 reader.BaseStream.Seek(idBaseStart + i, SeekOrigin.Begin);
                 reader.Read(test, 0, 4);
-                Console.WriteLine("officer: " + (officerDict[reader.ReadInt32()]));
+                Console.WriteLine("officer: " + officersDict[reader.ReadInt32()]);
+
+                reader.BaseStream.Seek(levelBaseStart + i, SeekOrigin.Begin);
+                reader.Read(test, 0, 4);
+                Console.WriteLine("level: " + reader.ReadInt32());
 
                 reader.BaseStream.Seek(xpBaseStart + i, SeekOrigin.Begin);
                 reader.Read(test, 0, 4);
@@ -119,10 +248,6 @@ class Program
                 reader.Read(test, 0, 4);
                 Console.WriteLine("kills: " + reader.ReadInt32());
 
-                reader.BaseStream.Seek(levelBaseStart + i, SeekOrigin.Begin);
-                reader.Read(test, 0, 4);
-                Console.WriteLine("level: " + reader.ReadInt32());
-
                 reader.BaseStream.Seek(titleBaseStart + i, SeekOrigin.Begin);
                 reader.Read(test, 0, 4);
                 Console.WriteLine("title: " + reader.ReadInt32());
@@ -131,80 +256,46 @@ class Program
                 reader.Read(test, 0, 4);
                 Console.WriteLine("costume: " + reader.ReadInt32());
 
-
                 reader.BaseStream.Seek(weapon1BaseStart + i, SeekOrigin.Begin);
                 reader.Read(test, 0, 4);
-                Console.WriteLine("weapon 1: " + reader.ReadInt32());
+                Console.WriteLine("weapon 1: " + weaponsDict[reader.ReadInt32()]);
 
                 reader.BaseStream.Seek(weapon2BaseStart + i, SeekOrigin.Begin);
                 reader.Read(test, 0, 4);
-                Console.WriteLine("weapon 2: " + reader.ReadInt32());
+                Console.WriteLine("weapon 2: " + weaponsDict[reader.ReadInt32()]);
 
                 reader.BaseStream.Seek(weapon3BaseStart + i, SeekOrigin.Begin);
                 reader.Read(test, 0, 4);
-                Console.WriteLine("weapon 3: " + reader.ReadInt32());
+                Console.WriteLine("weapon 3: " + weaponsDict[reader.ReadInt32()]);
 
                 reader.BaseStream.Seek(weapon4BaseStart + i, SeekOrigin.Begin);
                 reader.Read(test, 0, 4);
-                Console.WriteLine("weapon 4: " + reader.ReadInt32());
+                Console.WriteLine("weapon 4: " + weaponsDict[reader.ReadInt32()]);
 
                 reader.BaseStream.Seek(weapon5BaseStart + i, SeekOrigin.Begin);
                 reader.Read(test, 0, 4);
-                Console.WriteLine("weapon 5: " + reader.ReadInt32());
+                Console.WriteLine("weapon 5: " + weaponsDict[reader.ReadInt32()]);
 
                 reader.BaseStream.Seek(weapon6BaseStart + i, SeekOrigin.Begin);
                 reader.Read(test, 0, 4);
-                Console.WriteLine("weapon 6: " + reader.ReadInt32());
+                Console.WriteLine("weapon 6: " + weaponsDict[reader.ReadInt32()]);
 
                 reader.BaseStream.Seek(weapon7BaseStart + i, SeekOrigin.Begin);
                 reader.Read(test, 0, 4);
-                Console.WriteLine("weapon 7: " + reader.ReadInt32());
+                Console.WriteLine("weapon 7: " + weaponsDict[reader.ReadInt32()]);
 
                 reader.BaseStream.Seek(weapon8BaseStart + i, SeekOrigin.Begin);
                 reader.Read(test, 0, 4);
-                Console.WriteLine("weapon 8: " + reader.ReadInt32());
+                Console.WriteLine("weapon 8: " + weaponsDict[reader.ReadInt32()]);
 
 
                 Console.WriteLine("********* \n\n");
 
                 i = i + 167;
+                // 168 bytes per officer 
+                // fyi - save.dat bytes 2904 to 9791
+                //       6888 / 168 = 41 :)
             }
-
-   
-
-            /*
-            reader.BaseStream.Seek(4736, SeekOrigin.Begin);     // where to start reading from 
-            reader.Read(test, 0, 4);                            // number of bytes to read
-            int zhaoYunKillsVal = reader.ReadInt32(); // this has two extra bytes of 0's so might be stored as an int32 to allow for huge values
-
-            reader.BaseStream.Seek(4732, SeekOrigin.Begin);
-            reader.Read(test, 0, 4);
-            int zhaoYunXP = reader.ReadInt32(); // also a 32bit int
-
-            reader.BaseStream.Seek(4726, SeekOrigin.Begin);
-            reader.Read(test, 0, 2);
-            int zhaoYunTitle = reader.ReadInt16();
-
-            reader.BaseStream.Seek(4730, SeekOrigin.Begin);
-            reader.Read(test, 0, 2);
-            int zhaoYunLevel = reader.ReadInt16();
-
-            // offset between XP = 168????
-            reader.BaseStream.Seek(4900, SeekOrigin.Begin);
-            reader.Read(test, 0, 4);
-            int guanYuXP = reader.ReadInt32();
-
-            // 5068 = next officer xp??
-            reader.BaseStream.Seek(5068, SeekOrigin.Begin);
-            reader.Read(test, 0, 4);
-            int zhangFeiXP = reader.ReadInt32();
-        
-
-            reader.BaseStream.Seek(5236, SeekOrigin.Begin);
-            reader.Read(test, 0, 4);
-            int zhugeLiangXP = reader.ReadInt32();
-            */
-
 
         }
 
@@ -219,9 +310,8 @@ class Program
         //    Console.Write(hex);   
         //}
 
-        
-        while (true) ;
 
+        while (true) ;
     }
 
     static string DecodeOfficerId(int id)
@@ -232,11 +322,7 @@ class Program
         else
             return "idk";
     }
-
-    static string DecodeOfficerTitle(int title)
-    {
-        return "idk";
-    }
 }
 
+   
 
